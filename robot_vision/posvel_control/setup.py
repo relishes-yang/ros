09fb4999f1a5ac055ec5c5b6baf6e0faa0b3c0fe
relishes-yang <232058740+relishes-yang@users.noreply.github.com>
2026-06-03 22:08:09 +0800

    Delete robot_vision directory

--- robot_vision/posvel_control/setup.py
+++
@@ -1,8 +0,0 @@
-from catkin_pkg.python_setup import generate_distutils_setup
-from setuptools import setup
-
-d = generate_distutils_setup(
-    packages=['posvel_control'],
-    package_dir={'': '.'},  # ✅ 关键：使用当前目录（.）
-)
-setup(**d)
